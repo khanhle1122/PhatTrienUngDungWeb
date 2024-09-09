27a20afa1924a3ed0e015aba7d8a30a9a1ef6884
khanhle1122 <khanh300kkz@gmail.com>
2024-09-09 22:24:23 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require ("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
+const app = express();
+const ApiError = require("./app/api-error");
 const app = express();
 
+app.use("/api/contacts",contactsRouter);
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
+app.use("/api/contacts", contactsRouter);
 app.get("/",(req,res)=>{
 	res.json({message:"Welcome to contactbook application."});
 });
